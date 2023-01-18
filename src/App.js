4dbd7cbe3072ebbc55ffae9ab4f442e5411c20cf
Rafal Chojnowski <chojnowskigr@gmail.com>
2023-01-18 17:56:07 +0100

    select db

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { FetchDB, SelectDBWrapper, Table } from './table/table';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+    <SelectDBWrapper>
+      {({ db }) => <FetchDB db={db}>
+        {({ data }) =>
+          <Table data={data} />
+        }
+      </FetchDB>}
+    </SelectDBWrapper>
+  </>
+
 }
 
 export default App;
