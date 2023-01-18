7f536c1a95c591ca8da4ca0687900ee248f40022
Rafał Chojnowski <chojnowskigr@gmail.com>
2023-01-18 19:43:22 +0100

    feat

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import { FetchDB, SelectDBWrapper, Table } from './table/table';
+import logo from "./logo.svg";
+import "./App.css";
+import { FetchDB, SelectDBWrapper, Table, UserForm } from "./table/table";
 
 function App() {
-  return <>
-    <SelectDBWrapper>
-      {({ db }) => <FetchDB db={db}>
-        {({ data }) =>
-          <Table data={data} />
-        }
-      </FetchDB>}
-    </SelectDBWrapper>
-  </>
-
+  return (
+    <>
+      <SelectDBWrapper>
+        {({ db }) => (
+          <>
+            <FetchDB db={db}>{({ data }) => <Table data={data} />}</FetchDB>
+            <UserForm db={db} />
+          </>
+        )}
+      </SelectDBWrapper>
+    </>
+  );
 }
 
 export default App;
--- src/table/table.jsx
+++ src/table/table.jsx
@@ -1,95 +1,122 @@
-import React, {useState} from 'react';
-import { useEffect } from 'react';
-import {useForm} from 'react-hook-form'
-import axios from 'axios';
-import { apiurl } from '../consts';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { apiurl } from "../consts";
 
-export const SelectDBWrapper = ({children}) => {
-    const [db, setDB] = useState('/');
+export const SelectDBWrapper = ({ children }) => {
+  const [db, setDB] = useState("/");
 
-    const onChange = (e) => {
-        setDB(e.target.value)
-    }
+  const onChange = (e) => {
+    setDB(e.target.value);
+  };
 
-    return <>
-        <select value={db} onChange={onChange}>
-            <option value='/mongoose/'>Nierelacyjna</option>
-            <option value='/realm/'>Obiektowa</option>
-            <option value='/'>Relacyjna</option>
-        </select>
-        {children({db})}
+  return (
+    <>
+      <select value={db} onChange={onChange}>
+        <option value="/mongoose/">Nierelacyjna</option>
+        <option value="/realm/">Obiektowa</option>
+        <option value="/">Relacyjna</option>
+      </select>
+      {children({ db })}
     </>
-}   
+  );
+};
 
-export const FetchDB = ({db, children}) => {
+export const FetchDB = ({ db, children }) => {
+  const [data, setData] = useState();
 
-    const [data, setData] = useState();
+  useEffect(() => {
+    axios.get(`${apiurl}${db}users`).then((res) => setData(res.data));
+  }, [db]);
 
+  return children({ data });
+};
 
-    useEffect(() => {
-        axios.get(`${apiurl}${db}users`).then(res => setData(res.data))
-    }, [db])
+export const Table = ({ data }) => {
+  const li = ({ email, login }) => (
+    <li key={email}>
+      {email}
+      {login}
+    </li>
+  );
 
-    return children({data})
-}
+  const map = data?.map(li);
 
-
-
-export const Table = ({data}) => {
-
-    const li = ({email, login}) => <li key={email}>{email}{login}</li>
-
-    const map = data?.map(li);
-
-    return <ul>
+  return (
+    <>
+      <ul>
         {map}
         TEST
-    </ul>
-
-
-}
-
-export const ActionDelete = () => {
-    const [zip, setZip] = useState();
-
-    const onClick = () => {
-        // request(zip)
-    }
-
-    const onChange = (e) => {
-        setZip(e.target.value)
-    }
-
-    return <>
-    <input type="number" onChange={onChange}>ZIP CODE</input>
-     <button onClick={onClick}>Usuń po ZIP_CODE</button>
+      </ul>
     </>
-}
+  );
+};
 
-export const UserForm = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const [iterations, setIterations] = useState();
+export const ActionDelete = ({ db }) => {
+  const [zip, setZip] = useState();
 
-    const onChange = (e) => {
-        setIterations(e.target.value)
-    } 
+  const onClick = () => {
+    axios.delete(`${apiurl}${db}users/${zip}`).then((res) => {});
+  };
 
-    return <>
-    <input type='number' placeholder="Ilość rekordów" onChange={onChange}></input>
-     <form>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue='login' {...register("login", { required: true })} />
-      
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("email", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
-      
-      <input defaultValue='street' {...register("street")} />
-      <input defaultValue='zipCode' {...register("zipCode")} />
-      <input defaultValue='country' {...register("country")} />
-      <input type="submit" />
-    </form>
+  const onChange = (e) => {
+    setZip(e.target.value);
+  };
+
+  return (
+    <>
+      <input type="number" onChange={onChange}>
+        ZIP CODE
+      </input>
+      <button onClick={onClick}>Usuń po ZIP_CODE</button>
     </>
+  );
+};
 
-}
+export const UserForm = ({ db }) => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const [iterations, setIterations] = useState();
+
+  const onChange = (e) => {
+    setIterations(e.target.value);
+  };
+
+  const onSubmit = (data) => {
+    axios
+      .post(`${apiurl}${db}users/`, Array(iterations).fill(data))
+      .then((res) => {});
+  };
+
+  return (
+    <>
+      <input
+        type="number"
+        placeholder="Ilość rekordów"
+        onChange={onChange}
+      ></input>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <input
+          defaultValue="login"
+          {...register("login", { required: true })}
+        />
+
+        {/* include validation with required or other standard HTML validation rules */}
+        <input {...register("email", { required: true })} />
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired && <span>This field is required</span>}
+
+        <input defaultValue="street" {...register("street")} />
+        <input defaultValue="zipCode" {...register("zipCode")} />
+        <input defaultValue="country" {...register("country")} />
+        <input type="submit" />
+      </form>
+    </>
+  );
+};
